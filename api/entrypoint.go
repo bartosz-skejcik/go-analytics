dc61cc3b85a1cd5db4b7d21f99fd4f5b24f6fe4e
Bartek Paczesny <bartek@paczesny.pl>
2024-09-17 19:58:36 +0200

    backup:

--- api/entrypoint.go
+++ api/entrypoint.go
@@ -51,7 +51,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
     if db == nil {
         psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
             "password=%s dbname=%s sslmode=%s",
-            os.Getenv("POSTGRES_HOST"), 5432, os.Getenv("POSTGRES_USER"),
+            os.Getenv("POSTGRES_HOST"), 5333, os.Getenv("POSTGRES_USER"),
             os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DATABASE"),
             os.Getenv("POSTGRES_SSLMODE"))
 
